68ab7a8aaeb47c42e6217cca093773997fb00d66
GekkeGlenn-Dev <glenn.dev@hotmail.com>
2022-05-16 20:16:53 +0200

    Init project

---
+++ TrackYourPresence/TrackYourPresence/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TrackYourPresence.Services;
+using TrackYourPresence.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrackYourPresence
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TrackYourPresence/TrackYourPresence/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TrackYourPresence.ViewModels;
+using Xamarin.Forms;
+
+namespace TrackYourPresence.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
